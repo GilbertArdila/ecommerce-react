cf6dda44608653351d01a34ad48010d786eb886a
Gilbert Ardila <gilbertferney@gmail.com>
2022-10-04 09:29:31 -0500

    refactorizamos recovery-emmail, un pequeño error

--- src/Pages/PasswordRecovery/PasswordRecover.jsx
+++ src/Pages/PasswordRecovery/PasswordRecover.jsx
@@ -3,25 +3,18 @@ import './PasswordRecover.css';
 
 const PasswordRecover = () => {
   return (
-    <div className="recover">
-    <div className="form-container">
-      <img src="logo_yard_sale.svg" alt="logo" className="logo"/>
-
-      <h1 className="title">Email has been sent!</h1>
-      <p className="subtitle">Please check your inbox for instructions on how to reset the password</p>
-
-      <div className="email-image">
-        <img src="email.svg" alt="email"/>
-      </div>
-
-      <button className="primary-button login-button">Login</button>
-
-      <p className="resend">
-        <span>Didn't receive the email?</span>
-        <a href="/">Resend</a>
-      </p>
-    </div>
-  </div>
+    <div className="PasswordRecovery">
+			<div className="PasswordRecovery-container">
+				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+				<h1 className="title">Password recovery</h1>
+				<p className="subtitle">Inform the email address used to create your account</p>
+				<form action="/" className="form">
+					<label htmlFor="email" className="label">Email address</label>
+					<input type="text" id="email" className="input input-email" />
+					<input type="submit" value="Confirm" className="primary-button login-button" />
+				</form>
+			</div>
+		</div>
   )
 }
 
