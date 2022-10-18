2f632e9f2f77a4af294ac8519c842a0dfd92feb2
Gilbert Ardila <gilbertferney@gmail.com>
2022-10-18 15:56:27 -0500

    mejoramos lógica en order.jsx para mostrar aviso cuando esté vacío el carrito de compra

--- src/Components/Order/Order.jsx
+++ src/Components/Order/Order.jsx
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import { AppContext } from '../../Context/AppContext';
 import { handleTotalAmount } from '../../Helpers/handleTotalAmount';
@@ -6,39 +6,44 @@ import Swal from 'sweetalert2';
 
 
 const Order = () => {
-	const{state,addToCategorySearch}=useContext(AppContext);
-	const{cart}=state;
-	const date=new Date();
-	const history=useHistory();
-	
-	const handleClick=()=>{
-        Swal.fire({
+	const { state, addToCategorySearch } = useContext(AppContext);
+	const { cart } = state;
+	const date = new Date();
+	const history = useHistory();
+
+	const handleClick = () => {
+		Swal.fire({
 			title: 'Gracias por utilizar nuestros servicios :)',
 			showClass: {
-			  popup: 'animate__animated animate__fadeInDown'
+				popup: 'animate__animated animate__fadeInDown'
 			},
 			hideClass: {
-			  popup: 'animate__animated animate__fadeOutUp'
+				popup: 'animate__animated animate__fadeOutUp'
 			}
-		  })
-		  addToCategorySearch('');
-		  cart.length=0;
-		  history.push('/')
+		})
+		addToCategorySearch('');
+		cart.length = 0;
+		history.push('/')
 	}
-  return (
-	<>
-    <div className="Order">
-			<p>
-				<span>{date.toISOString().split('T')[0]}</span>
-				<span>{cart.length} articles</span>
-			</p>
-			<p>${handleTotalAmount()}</p>
-			<img src="flechita.svg" alt="arrow" />
-			
-		</div>
-		<button className='Order-button' onClick={handleClick}>Pagar</button>
+	return (
+		<>
+
+			<div className="Order">
+				{cart.length > 0 ?
+					<>
+						<p>
+							<span>{date.toISOString().split('T')[0]}</span>
+							<span>{cart.length} articles</span>
+						</p>
+						<p>${handleTotalAmount()}</p>
+						<img src="flechita.svg" alt="arrow" /></>:<h2>Aún no hay ningun producto en el carrito</h2>
+					
+				}
+			</div>
+			{cart.length > 0 &&
+				<button className='Order-button' onClick={handleClick}>Pagar</button>}
 		</>
-  )
+	)
 }
 
-export  {Order}
+export { Order }
